111eb3aafca41195e466e9418f4291790cd86d65
Dmitry <dmitry91ivanov@yandex.ru>
2024-03-13 09:44:06 +0300

    Задание выполнено

--- src/CompareArray.java
+++ src/CompareArray.java
@@ -1,2 +1,32 @@
-public class CompareArray {
+import java.util.ArrayList;
+import java.util.Objects;
+
+/*
+2. Напишите обобщенный метод compareArrays(), который принимает два массива и возвращает true,
+ если они одинаковые, и false в противном случае. Массивы могут быть любого типа данных,
+ но должны иметь одинаковую длину и содержать элементы одного типа попарно по индексам.
+ */
+public class CompareArray<T1, T2> {
+    public static class Pen {
+
+    }
+    private ArrayList<T1> array1 = new ArrayList<>();
+    private ArrayList<T2> array2 = new ArrayList<>();
+    public  <T> boolean compareArrays(ArrayList<T1> array1, ArrayList<T2> array2){
+        if(array1.size() != array1.size()) {
+            System.out.println("Неправильно (разная длина)");
+            return false;
+        }
+        else {
+            for(int i = 0; i < array1.size(); i++) {
+                if (!array1.get(i).getClass().getSimpleName().equals(array2.get(i).getClass().getSimpleName())) {
+                    System.out.println("Неправильно (разные типы)");
+                    return false;
+                }
+            }
+            System.out.println("Массивы имеют одинаковую длину, типы элементов попарно равны");
+            return true;
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+
         // Задание 1 (Calculator)
         double a = 10.254;
         int b = 15;
@@ -10,7 +11,39 @@ public class Main {
         System.out.println("Произведение: " + Calculator.multiply(a, b));
         System.out.println("Частное: " + Calculator.divide(a, b));
         System.out.println("Частное: " + Calculator.subtract(a, b));
+        System.out.println("=====================================");
 
+        // Задание 2 (compareArrays())
+        ArrayList<Object> str1 = new ArrayList<>();
+        ArrayList<Object> str2 = new ArrayList<>();
+        CompareArray.Pen pen1 = new CompareArray.Pen();
+        CompareArray.Pen pen2 = new CompareArray.Pen();
+        String el1 = "1";
+        int el2 = 2;
+        int el3 = 3;
+        String el4 = "4";
+        double el5 = 7;
+        double el6 = 1893;
+
+        str1.add(el1);
+        str1.add(el3);
+        str1.add(el5);
+        str1.add(pen1);
+
+        str2.add(el4);
+        str2.add(el2);
+        str2.add(el6);
+        str2.add(pen2);
+
+        CompareArray <Object, Object> compareArray = new CompareArray<>();
+        System.out.println(compareArray.compareArrays(str1, str2));
+        System.out.println("=====================================");
+
+        // Задание 3 (Pair)
+        Pair<String, Double> pair = new Pair<>("Первый", 2.22);
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+        System.out.println(pair.toString());
 
     }
 }
